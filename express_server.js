e3102c18cdb38186d81291edb716696757381dcf
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-01-25 13:39:39 -0500

    updated

--- express_server.js
+++ express_server.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+
 const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 
-function generateRandomString() {
+app.use(bodyParser.urlencoded({extended: true}));
 
-}
+function generateRandomString() {
+  let num = (Math.random() + 1).toString(36).substring(7);
+//console.log("random", num);
+return num;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const shortUrl = generateRandomString();
+  console.log("body longURL: ", req.body.longURL);
+  urlDatabase[shortUrl] = req.body.longURL;
+  res.redirect(`/urls/${shortUrl}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -33,6 +39,12 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(urlDatabase);
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/", (req, res) => {
